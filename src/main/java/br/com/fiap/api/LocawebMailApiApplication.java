83f1d034c0daeae6ef7ac5afa09476a97b9a27c7
Hadryan Silva <hadryan2001@gmail.com>
2024-09-09 20:37:41 -0300

    initial commit

---
+++ src/main/java/br/com/fiap/api/LocawebMailApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocawebMailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocawebMailApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/api/LocawebMailApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocawebMailApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
